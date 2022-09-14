f40b2b95b87bd0f58283bc7625fd56df0c4d71fe
jaejoon.han <jaejoon.han@crevisse.com>
2022-09-14 23:38:38 +0900

    전송 준비

--- WebCameraControl/Views/Index/Index.cshtml.cs
+++ WebCameraControl/Views/Index/Index.cshtml.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebCameraControl.Pages
+namespace WebCameraControl.Pages.Index
 {
     public class IndexModel : PageModel
     {
