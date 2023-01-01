6624961479188bc1570abb22d678f7f98e74e989
jaejoon.han <jaejoon.han@crevisse.com>
2023-01-01 14:41:10 +0900

    프린팅 페이지 연결

--- WebCameraControl/Controllers/ImageController.cs
+++ WebCameraControl/Controllers/ImageController.cs
@@ -22,6 +22,86 @@ public class ImageController : Controller
         _environment = environment;
     }
 
+    [HttpPost("print")]
+    public async Task<RedirectResult> PrintImage(string imageSourceBase64)
+    {
+        string? printerEmail = _configuration["PrinterEmail"];
+
+        if (string.IsNullOrEmpty(imageSourceBase64) || string.IsNullOrEmpty(printerEmail))
+        {
+            throw new Exception("잘못된 접근입니다.");
+        }
+
+        byte[] resultBytes = Convert.FromBase64String(imageSourceBase64);
+
+        // DB 저장
+        string downloadKey = Guid.NewGuid().ToString();
+        string link;
+
+        try
+        {
+            _appDbContext.UserFiles.Add(new UserFile
+            {
+                Id = _appDbContext.UserFiles.ToList().Count + 1,
+                Email = printerEmail,
+                DownloadKey = downloadKey,
+                Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                Content = resultBytes
+            });
+
+            await _appDbContext.SaveChangesAsync();
+        }
+        catch
+        {
+            throw new Exception("DB 저장 실패");
+        }
+
+        try
+        {
+            // 최종 결과
+            MemoryStream image = new(resultBytes);
+
+            link = $"{Request.GetUri().GetLeftPart(UriPartial.Authority)}/download/{downloadKey}";
+
+            MailMessage newMail = new();
+
+            newMail.From = new MailAddress(_configuration["EmailSenderAddress"] ?? string.Empty,
+                _configuration["EmailSenderName"]);
+            newMail.To.Add(printerEmail);
+
+            // 이미지 첨부를 위한 처리
+            const string body = $@"<img src=""cid:HaruHaruPicture"" style=""width: 540px; height: 360px;"" alt=""image"" />";
+            AlternateView alternateView = AlternateView.CreateAlternateViewFromString(body, null, MediaTypeNames.Text.Html);
+
+            LinkedResource imageSource = new(image, MediaTypeNames.Image.Jpeg);
+            imageSource.ContentId = "HaruHaruPicture";
+            imageSource.ContentType = new ContentType("image/jpg");
+            alternateView.LinkedResources.Add(imageSource);
+            newMail.AlternateViews.Add(alternateView);
+
+            ContentType mimeType = new("text/html");
+            AlternateView alternate = AlternateView.CreateAlternateViewFromString(body, mimeType);
+            newMail.AlternateViews.Add(alternate);
+
+            newMail.Subject = "[HaruHaru] pictures";
+            newMail.Body = body;
+
+            SmtpClient client = new("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(_configuration["GmailUser"], _configuration["GmailPassword"]),
+                EnableSsl = true,
+            };
+
+            client.Send(newMail);
+        }
+        catch
+        {
+            throw new Exception("Email 전송 실패");
+        }
+
+        return Redirect("printing");
+    }
+
     [HttpPost("send")]
     public async Task<JsonResult> SendImages(SendImagesCommand command)
     {
