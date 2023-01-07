a3b62f8fb8145670e4a522c9f743f20b059d11fc
may <53140703+soo-toance@users.noreply.github.com>
2023-01-07 21:03:24 +0900

    css 수정  (#13)
    * 너비 / 높이 조정 기능 추가
    
    * 카메라 css 조정 및 사진찍는 화면에서 마우스 클릭시 사진 찍히도록 기능 추가
    
    * 사진 프리뷰 기능 만들기
    
    * 사진 선택시 border 기능 추가
    
    * 프리뷰 기능 개발
    
    * 사진 선택 화면 개발
    
    * #6. 필터 타입 / 백그라운드 설정 화면 내 버튼 기능 연동
    
    * 타이머 영역 잡아두기
    
    * 사진 선택 타이머 구현
    
    * 화이트 프레임 css 추가
    
    * 파비콘 아이콘 변경
    
    * 메인 화면 디자인 추가
    
    * Lexend Deca 폰트 적용
    
    * 찰칵 이모지 적용
    
    - https://codepen.io/GeorgePark/pen/aazBGj
    
    * 카운트다운 넣는 부분 추가 향후 타이밍 맞춰서 넣도록 기능 추가해야함
    
    * 사진 선택 화면에서 이미지로 바꾸도록 기능 추가
    
    * 사진 선택시 넘버링 기능 추가
    
    * 선택한 순서대로 화면에 표시되도록 수정
    
    * 배경음 넣기
    
    * 사진 프리뷰 너비 / 높이 맞추기
    
    * 사진 선택 화면 css 조정
    
    * 메인화면 css 수정
    
    * 가격 선택 화면 css 수정 / 관리자 화면 삭제
    
    * 가이드 화면 css 적용
    
    * 사진 찍는 화면 css 수정
    
    * 사진 찍는 css 수정
    
    * 사진선택쪽 css 수정

--- WebCameraControl/Core/CheckPassword.cs
+++ WebCameraControl/Core/CheckPassword.cs
@@ -9,8 +9,9 @@ public sealed class CheckLoginAttribute : Attribute, IAuthorizationFilter
     {
         if (context.HttpContext.Session.GetString("IsPasswordChecked") != "1")
         {
+            // 관리자 인증 부분 생략 , 향후 관리자 인증 넣을시 여기 다시 주석해제애야함
             // context.Result = new UnauthorizedResult();
-            throw new Exception("Unauthorized!");
+            // throw new Exception("Unauthorized!");
         }
     }
 }
